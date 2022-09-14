ea5d9e6704506331d140059b98344eba6e9a81d3
tjsherrill <tj@ravennainteractive.com>
2022-09-14 11:37:34 -0700

    update blade component with correct data and store names

--- src/resources/views/components/nautilus-login.blade.php
+++ src/resources/views/components/nautilus-login.blade.php
@@ -1,5 +1,5 @@
-<div class="text-center" x-data="nautilusLogin">
-    <template x-if="Alpine.store('nautilusStatus').getConnected() == false">    
+<div class="text-center" x-data="terahertzLogin">
+    <template x-if="Alpine.store('terahertzStore').getConnected() == false">    
         <button 
             class="bg-blue-900 text-white px-5 py-2.5 mr-2 mb-2" 
             x-bind="connectWallet"> 
@@ -7,7 +7,7 @@
         </button>     
     </template>
 
-    <template x-if="Alpine.store('nautilusStatus').getConnected() == false">    
+    <template x-if="Alpine.store('terahertzStore').getConnected() == false">    
         <button 
         class="bg-blue-900 text-white px-5 py-2.5 mr-2 mb-2" 
         x-bind="login"> 
@@ -15,9 +15,9 @@
         </button>     
     </template>
 
-    <template x-if="Alpine.store('nautilusStatus').getConnected() == 'true'">    
+    <template x-if="Alpine.store('terahertzStore').getConnected() == 'true'">    
         <p>
-            Wallet Address: <small><span x-text="Alpine.store('nautilusStatus').getAddress()"></span></small><br>
+            Wallet Address: <small><span x-text="Alpine.store('terahertzStore').getAddress()"></span></small><br>
             Wallet Balance: <span x-text="ergBalance"></span><br>
             <a href="" x-bind="disconnectWallet" class="text-rose-900 underline underline-offset-1">Disconnect</a>
         </p>
